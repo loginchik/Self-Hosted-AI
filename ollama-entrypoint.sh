5270241d5e90aa1a5d98630b661d134fdbac90f4
Anna Loginova <zv1376a@gmail.com>
2025-04-27 21:43:44 +0300

    fix: replace ollama run with ollama pull command

--- ollama-entrypoint.sh
+++ ollama-entrypoint.sh
@@ -5,13 +5,13 @@ ollama serve
 # Uncomment lines `ollama run...` to automatically install the models
 
 # https://ollama.com/library/gemma3 - 1b = 815MB
-# ollama run gemma3:1b
+# ollama pull gemma3:1b
 
 # https://ollama.com/library/deepseek-r1 - 7b = 4.7BG
-# ollama run deepseek-r1:7b
+# ollama pull deepseek-r1:7b
 
 # https://ollama.com/library/llama3.2 - 3b = 2GB
-# ollama run llama3.2:3b
+# ollama pull llama3.2:3b
 
 # https://ollama.com/library/phi3 - 3.8b = 2.2GB
-# ollama run phi3:3.8b
+# ollama pull phi3:3.8b
